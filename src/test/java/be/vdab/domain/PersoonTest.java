d0c52aea5a7ebf0b6c978117da3c00bc5ba8c016
katia_biryla <kbiril@gmail.com>
2023-03-31 10:45:58 +0200

    Kenneth aub

--- src/test/java/be/vdab/domain/PersoonTest.java
+++ src/test/java/be/vdab/domain/PersoonTest.java
@@ -6,10 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-
 
 class PersoonTest {
     private Persoon persoon;
@@ -29,9 +25,10 @@ class PersoonTest {
     }
 
     @Test
-    void addAndereVoornaamMag() {
+    void addAndereVoornamenMag() {
         persoon.add("Raf");
-        assertThat(persoon).hasToString("Pieter Jan Raf");
+        persoon.add("Henry");
+        assertThat(persoon).hasToString("Pieter Jan Raf Henry");
     }
 
     @Test
